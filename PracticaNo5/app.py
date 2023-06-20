ff5caddabe7d6b6c847bed315a1a49d3bb3f7f80
MayaGaS <11mayagarrido.11@gmail.com>
2023-06-20 15:19:37 -0600

    Practica 6: Vistas, Envío Información, Vista u Rutas

--- PracticaNo5/app.py
+++ PracticaNo5/app.py
@@ -1,34 +1,39 @@
-
-from flask import Flask
-
-#Inicializacion del servidor Flask
-app = Flask(__name__)
-
-#Configuracion para la conexion a BD
-app.config['MYSQL_HOST'] = "localhost"
-app.config['MYSQL_USER'] = "root"
-app.config['MYSQL_PASSWORD'] = " "
-app.config['MYSQL_DB'] = "dbflask"
+from flask import Flask, render_template, request
 
 
-#Declaracion de rutas
+#inicialización del servidor Flask
+app= Flask(__name__)
+app.config['MYSQL_HOST']= "localhost"
+app.config['MYSQL_USER']= "root"
+app.config['MYSQL_PASSWORD']= ""
+app.config['MYSQL_DB']= "dbflask"
 
+#--- Declaración de rutas ---
 
-#Ruta Index http://localhost:5000
-#La rurta se compone de nombre y funcion 
+# Ruta index o ruta principal http://localhost:5000
+# La ruta se compone de nombre y función
+
 @app.route('/')
 def index():
-    return "Hola Mundo"
+    return render_template('index.html')
 
-@app.route('/guardar')
+@app.route('/guardar', methods=['POST'])
 def guardar():
-    return "Se guardo el album en la BD"
+    if request.method=='POST':
+        titulo= request.form['txtTitulo']
+        artista= request.form['txtArtista']
+        anio= request.form['txtAnio']
+        print(titulo, artista, anio)
+
+    return "La info del Album llego a su ruta Amigo ;)"
 
 @app.route('/eliminar')
 def eliminar():
-    return "Se elimino el album en la BD"
+    return "Se eliminó el album en la BD"
 
 
-#Ejecucion de servidor 
-if __name__ == '__main__':
-    app.run(port = 5000, debug = True) 
+
+
+# Ejecución de servidor
+if __name__== '__main__':
+    app.run(port= 5000, debug=True)

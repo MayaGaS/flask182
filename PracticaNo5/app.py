bc002a896441f304c8a93d0eb06e972159c81b10
MayaGaS <11mayagarrido.11@gmail.com>
2023-06-20 18:54:34 -0600

    Inserta la base de datos, redirect y mensajes de la ruta

--- PracticaNo5/app.py
+++ PracticaNo5/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_mysqldb import MySQL
 
 #inicialización del servidor Flask
 app= Flask(__name__)
@@ -7,6 +7,10 @@ app.config['MYSQL_HOST']= "localhost"
 app.config['MYSQL_USER']= "root"
 app.config['MYSQL_PASSWORD']= ""
 app.config['MYSQL_DB']= "dbflask"
+
+app.secret_key = 'mysecretkey'
+
+mysql = MySQL(app)
 
 #--- Declaración de rutas ---
 
@@ -20,12 +24,18 @@ def index():
 @app.route('/guardar', methods=['POST'])
 def guardar():
     if request.method=='POST':
-        titulo= request.form['txtTitulo']
-        artista= request.form['txtArtista']
-        anio= request.form['txtAnio']
-        print(titulo, artista, anio)
+        Vtitulo= request.form['txtTitulo']
+        Vartista= request.form['txtArtista']
+        Vanio= request.form['txtAnio']
+        # print(titulo, artista, anio)
 
-    return "La info del Album llego a su ruta Amigo ;)"
+        CS = mysql.connection.cursor()
+        CS.execute('insert into albums(titulo, artista, anio) values(%s, %s, %s)', (Vtitulo, Vartista, Vanio))
+        mysql.connection.commit()
+        
+    flash('Album Agregado Correctamente')
+    return redirect(url_for('index'))
+
 
 @app.route('/eliminar')
 def eliminar():
